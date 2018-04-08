4a29096e2f523651dfcdd4c64203ec97e8fc20c0
WCNGS <WCNGS@QQ.COM>
2018-04-08 15:43:42 +0800

    修改格式规范

--- es-common/src/main/java/xyz/wongs/es/modules/sys/service/LogService.java
+++ es-common/src/main/java/xyz/wongs/es/modules/sys/service/LogService.java
@@ -21,8 +21,9 @@ import xyz.wongs.es.modules.sys.entity.Log;
 @Transactional(readOnly = true)
 public class LogService extends CrudService<LogDao, Log> {
 
+	@Override
 	public Page<Log> findPage(Page<Log> page, Log log) {
-		
+
 		// 设置默认时间范围，默认当前月
 		if (log.getBeginDate() == null){
 			log.setBeginDate(DateUtils.setDays(DateUtils.parseDate(DateUtils.getDate()), 1));
@@ -30,9 +31,9 @@ public class LogService extends CrudService<LogDao, Log> {
 		if (log.getEndDate() == null){
 			log.setEndDate(DateUtils.addMonths(log.getBeginDate(), 1));
 		}
-		
+
 		return super.findPage(page, log);
-		
+
 	}
-	
+
 }
