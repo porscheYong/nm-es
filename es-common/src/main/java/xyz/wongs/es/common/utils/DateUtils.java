49ca456628b2b8cc9bcb87cff35b45c0d24a46bf
WCNGS <WCNGS@QQ.COM>
2018-05-31 14:37:44 +0800

    工具类：日期 添加之前由于版本冲突造成的丢失

--- es-common/src/main/java/xyz/wongs/es/common/utils/DateUtils.java
+++ es-common/src/main/java/xyz/wongs/es/common/utils/DateUtils.java
@@ -233,5 +233,95 @@ public class DateUtils extends org.apache.commons.lang3.time.DateUtils {
 		return convertSuccess;
 	}
 
+	/**
+	 * 根据输入字符串，转换日期
+	 * @method      getDatebystr
+	 * @author      WCNGS@QQ.COM
+	 * @version
+	 * @see
+	 * @param str
+	 * @param pattern
+	 * @return      java.util.Date
+	 * @exception
+	 * @date        2018/4/12 19:55
+	 */
+	public static Date getDatebystr(String str,String pattern){
 
+		if(str==null||str==""){
+			return null;
+		}
+		SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+		try {
+			return sdf.parse(str);
+		} catch (ParseException e) {
+			return null;
+		}
+	}
+	/**
+	 * 方法实现说明
+	 * @method      dateVali
+	 * @author      WCNGS@QQ.COM
+	 * @version
+	 * @see
+	 * @param date1
+	 * @param date2
+	 * @return      int
+	 * @exception
+	 * @date        2018/4/12 19:53
+	 */
+	public static int dateVali(Date date1,Date date2){
+
+		Calendar cal1 = Calendar.getInstance();
+		cal1.setTime(date1);
+
+		Calendar cal2 = Calendar.getInstance();
+		cal2.setTime(date2);
+		int day1= cal1.get(Calendar.DAY_OF_YEAR);
+		int day2 = cal2.get(Calendar.DAY_OF_YEAR);
+
+		int year1 = cal1.get(Calendar.YEAR);
+		int year2 = cal2.get(Calendar.YEAR);
+		//同一年
+		if(year1 != year2) {
+			int timeDistance = 0 ;
+			for(int i = year1 ; i < year2 ; i ++)  {
+				//闰年
+				if(i%4==0 && i%100!=0 || i%400==0) {
+					timeDistance += 366;
+				}else {
+					//不是闰年
+					timeDistance += 365;
+				}
+			}
+
+			return timeDistance + (day2-day1) ;
+		} else  {
+			return day2-day1;
+		}
+	}
+
+
+	/**
+	 * 方法实现说明
+	 * @method      dateVali
+	 * @author      WCNGS@QQ.COM
+	 * @version
+	 * @see
+	 * @param date1
+	 * @param date2
+	 * @return      int
+	 * @exception
+	 * @date        2018/4/12 19:53
+	 */
+	public static int dateValiSeconds(Date date1,Date date2){
+
+//		Calendar cal1 = Calendar.getInstance();
+
+		long time1 = date1.getTime();
+
+
+		long time2 = date2.getTime();
+		long between_days=(time2-time1)/(1000*3600*24);
+		return Integer.parseInt(String.valueOf(between_days));
+	}
 }
